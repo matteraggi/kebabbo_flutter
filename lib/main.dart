d6e995d1426eed294a90b6aad92c1382142d8f4d
Matteo Raggi <matteo.raggi04@gmail.com>
2024-06-21 10:36:14 +0200

    env

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kebabbo_flutter/pages/about_page.dart';
 import 'package:kebabbo_flutter/pages/account_page.dart';
 import 'package:kebabbo_flutter/pages/login_page.dart';
@@ -11,10 +12,16 @@ Color red = Color.fromRGBO(187, 0, 0, 1.0);
 Color yellow = Color.fromRGBO(255, 186, 28, 1.0);
 
 Future<void> main() async {
-  await Supabase.initialize(
-      url: 'https://ntrxsuhmslsvlflwbizb.supabase.co',
-      anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50cnhzdWhtc2xzdmxmbHdiaXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3OTAwNzYsImV4cCI6MjAzNDM2NjA3Nn0.lJ9AUgZteiVE7DVTLBCf7mUs5HhUK9EpefB9hIHeEFI');
+  await dotenv.load(fileName: "process.env");
+
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+  if (supabaseUrl == null || supabaseAnonKey == null) {
+    throw Exception("Supabase URL and Anon Key must be set in .env file");
+  }
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
   runApp(MyApp());
 }
 
